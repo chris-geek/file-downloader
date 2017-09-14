700cdd0805ce5284cf38acbf91f20c77cdb8d2c3
chris-geek <mac555@tiscali.it>
2017-09-14 08:27:58 +0200

    Added MD5 integrity checker

---
+++ src/main/java/com/github/phudekar/downloader/utils/IntegrityChecker.java
@@ -0,0 +1,34 @@
+package com.github.phudekar.downloader.utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class IntegrityChecker {
+	public String getFileMd5(String path) throws NoSuchAlgorithmException, IOException {
+	
+		MessageDigest md = MessageDigest.getInstance("MD5");
+		byte bytes[] = new byte[16836];
+		
+		try (InputStream is = Files.newInputStream(Paths.get(path));				
+				DigestInputStream dis = new DigestInputStream(is, md)) {
+			/* Read decorated stream (dis) to EOF as normal... */
+			while (dis.read(bytes) > 0 ) {}
+		}
+		
+		byte[] digest = md.digest();
+
+	    StringBuilder sb = new StringBuilder();
+	    
+	    for (byte b : digest) {
+	        sb.append(String.format("%02X", b));
+	    }
+	    
+	    return sb.toString();
+	}
+}
