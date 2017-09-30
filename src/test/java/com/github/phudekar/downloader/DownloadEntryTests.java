94f624f7b5f2ffee5618880b369f20856866ebb0
chris-geek <mac555@tiscali.it>
2017-09-30 09:21:06 +0200

    .

--- src/test/java/com/github/phudekar/downloader/DownloadEntryTests.java
+++ src/test/java/com/github/phudekar/downloader/DownloadEntryTests.java
@@ -12,7 +12,7 @@ public class DownloadEntryTests {
     @Test
     public void shouldSetDefaultFilenameForDownloadEntry() {
         String url = "https://raw.githubusercontent.com/phudekar/file-downloader/master/";
-        String location = "." + File.separatorChar + "bin";
+        String location = "." + File.separatorChar + "build" + File.separatorChar + "tmp";
 
         DownloadEntry downloadEntry = new DownloadEntry(url, location);
         assertThat(downloadEntry.getFile().getPath(), is(location + File.separatorChar + DownloadEntry.DEFAULT_FILE_NAME + DownloadEntry.PARTIAL_DOWNLOAD_SUFFIX));
